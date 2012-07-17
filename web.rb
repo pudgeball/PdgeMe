810c464971c70d55e2a9ec160901c046f807f14d
Nick McGuire <pudgeball.mcguire@gmail.com>
2012-07-17 14:30:31 -0400

    first real verion :D, updated the html, changed the redis store, and yeah

--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'redis'
 
 
-uri = URI.parse('redis://nickmcguire:3cd38c7b1847ae2e35cdcb878c22b063@chubb.redistogo.com:9261/')
+uri = URI.parse('redis://redistogo:b6a74f344b75d53eceb91b72f2f7fe64@chubb.redistogo.com:9259/')
 redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 helpers do
@@ -18,7 +18,7 @@ get '/' do
   erb :index
 end
 
-post "/" do
+post '/' do
   if params[:url] and not params[:url].empty?
     @shortcode = random_string(5)
     redis.setnx "links:#{@shortcode}", params[:url]
