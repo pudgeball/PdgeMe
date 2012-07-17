5f74e3be5d1b7a8f88b8c9a0c1b132d12a2b04cf
Nick McGuire <pudgeball.mcguire@gmail.com>
2012-07-17 18:29:00 -0400

    New Methods!
    Updated web.rb to include some awesome new methods
    Also worked on the style to include those new methods!

--- web.rb
+++ web.rb
@@ -2,12 +2,27 @@ require 'sinatra'
 require 'redis'
 
 
-uri = URI.parse('redis://redistogo:b6a74f344b75d53eceb91b72f2f7fe64@chubb.redistogo.com:9259/')
-redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
+  
+  def startRedis
+    uri = URI.parse('redis://redistogo:b6a74f344b75d53eceb91b72f2f7fe64@chubb.redistogo.com:9259/')
+    @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  end
+  
+  def size
+    startRedis
+    @redis.dbsize
+  end
+  
+  def setURL(url)
+    startRedis
+    @shortcode = random_string(5)
+    @redis.setnx "links:#{@shortcode}", url
+  end
   
   def random_string(length)
     rand(36**length).to_s(36)
@@ -20,13 +35,13 @@ end
 
 post '/' do
   if params[:url] and not params[:url].empty?
-    @shortcode = random_string(5)
-    redis.setnx "links:#{@shortcode}", params[:url]
+    setURL(params[:url])
   end
   erb :index
 end
 
 get '/:shortcode' do
+  startRedis()
   @url = redis.get "links:#{params[:shortcode]}"
   redirect @url || '/', 301
 end
